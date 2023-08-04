d899c885f28a00faf47f9cae73dabc352795000d
tHowl-1 <thomas.howland@verizon.net>
2023-08-03 23:41:16 -0400

    thomas stuff

---
+++ key_val.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <array>
+#include <memory>
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+
+#include "container.h"
+
+class AssociationList
+{
+private:
+    struct Node
+    {
+        Node(int key, TaxInfo container) : key(key), container(container), nextNode(nullptr) {}
+
+        const int key;
+        const TaxInfo container;
+           
+        Node* nextNode;
+    };
+
+    Node* headNode;
+
+public:
+    AssociationList();
+    ~AssociationList();
+
+    AssociationList(const AssociationList&) = delete;
+    AssociationList& operator= (const AssociationList&) = delete;
+
+    void print();
+
+    void push(int key, TaxInfo info);
+    std::pair<bool, TaxInfo> retrieve(const int key);
+
+    void createFromFile(std::fstream& file);
+};

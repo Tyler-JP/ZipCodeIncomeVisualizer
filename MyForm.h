0a434b126e3a8656df35774d6dc1a854ad04bc3a
Jason Chen <105614698+jnchen12a@users.noreply.github.com>
2023-08-05 12:02:51 -0400

    Made additional UI changes

--- MyForm.h
+++ MyForm.h
@@ -28,6 +28,13 @@ namespace Project3 {
 			//
 			hashMap = new HashMap();
 			keyPair = new AssociationList();
+			incomeRanges = gcnew array<String^>(6);
+			incomeRanges[0] = "<$25k";
+			incomeRanges[1] = "$25k-$50k";
+			incomeRanges[2] = "$50k-$75k";
+			incomeRanges[3] = "$75k-$100k";
+			incomeRanges[4] = "$100k-$200k";
+			incomeRanges[5] = ">$200k";
 		}
 
 	protected:
@@ -46,6 +53,7 @@ namespace Project3 {
 	private:
 		HashMap *hashMap;
 		AssociationList *keyPair;
+		array<String^>^ incomeRanges;
 
 	private: System::Windows::Forms::RadioButton^ keypairRadio;
 	private: System::Windows::Forms::RadioButton^ hashtableRadio;
@@ -90,8 +98,9 @@ namespace Project3 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			System::Windows::Forms::DataVisualization::Charting::ChartArea^ chartArea1 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
-			System::Windows::Forms::DataVisualization::Charting::Series^ series1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
+			System::Windows::Forms::DataVisualization::Charting::ChartArea^ chartArea4 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
+			System::Windows::Forms::DataVisualization::Charting::Legend^ legend4 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
+			System::Windows::Forms::DataVisualization::Charting::Series^ series4 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
 			this->keypairRadio = (gcnew System::Windows::Forms::RadioButton());
 			this->hashtableRadio = (gcnew System::Windows::Forms::RadioButton());
 			this->zipLabel = (gcnew System::Windows::Forms::Label());
@@ -111,7 +120,7 @@ namespace Project3 {
 			this->keypairRadio->Checked = true;
 			this->keypairRadio->Font = (gcnew System::Drawing::Font(L"Malgun Gothic", 18, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->keypairRadio->Location = System::Drawing::Point(39, 9);
+			this->keypairRadio->Location = System::Drawing::Point(966, 75);
 			this->keypairRadio->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
 			this->keypairRadio->Name = L"keypairRadio";
 			this->keypairRadio->RightToLeft = System::Windows::Forms::RightToLeft::Yes;
@@ -126,7 +135,7 @@ namespace Project3 {
 			// 
 			this->hashtableRadio->AutoSize = true;
 			this->hashtableRadio->Font = (gcnew System::Drawing::Font(L"Malgun Gothic", 18, System::Drawing::FontStyle::Bold));
-			this->hashtableRadio->Location = System::Drawing::Point(39, 60);
+			this->hashtableRadio->Location = System::Drawing::Point(966, 131);
 			this->hashtableRadio->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
 			this->hashtableRadio->Name = L"hashtableRadio";
 			this->hashtableRadio->RightToLeft = System::Windows::Forms::RightToLeft::Yes;
@@ -140,23 +149,29 @@ namespace Project3 {
 			// 
 			this->zipLabel->AutoSize = true;
 			this->zipLabel->Font = (gcnew System::Drawing::Font(L"Malgun Gothic", 18, System::Drawing::FontStyle::Bold));
-			this->zipLabel->Location = System::Drawing::Point(45, 114);
+			this->zipLabel->Location = System::Drawing::Point(139, 29);
 			this->zipLabel->Name = L"zipLabel";
 			this->zipLabel->Size = System::Drawing::Size(156, 41);
 			this->zipLabel->TabIndex = 9;
 			this->zipLabel->Text = L"Zip-Code:";
+			this->zipLabel->Click += gcnew System::EventHandler(this, &MyForm::zipLabel_Click);
 			// 
 			// zipChart
 			// 
-			chartArea1->Name = L"ChartArea1";
-			this->zipChart->ChartAreas->Add(chartArea1);
-			this->zipChart->Location = System::Drawing::Point(28, 314);
+			chartArea4->Name = L"ChartArea1";
+			this->zipChart->ChartAreas->Add(chartArea4);
+			legend4->AutoFitMinFontSize = 5;
+			legend4->Enabled = false;
+			legend4->Name = L"Legend1";
+			this->zipChart->Legends->Add(legend4);
+			this->zipChart->Location = System::Drawing::Point(27, 257);
 			this->zipChart->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
 			this->zipChart->Name = L"zipChart";
-			series1->ChartArea = L"ChartArea1";
-			series1->Name = L"Zipcode";
-			this->zipChart->Series->Add(series1);
-			this->zipChart->Size = System::Drawing::Size(443, 362);
+			series4->ChartArea = L"ChartArea1";
+			series4->Legend = L"Legend1";
+			series4->Name = L"Zipcode";
+			this->zipChart->Series->Add(series4);
+			this->zipChart->Size = System::Drawing::Size(1215, 491);
 			this->zipChart->TabIndex = 10;
 			this->zipChart->Text = L"chart1";
 			this->zipChart->Click += gcnew System::EventHandler(this, &MyForm::chart1_Click);
@@ -164,10 +179,10 @@ namespace Project3 {
 			// loadChart
 			// 
 			this->loadChart->Font = (gcnew System::Drawing::Font(L"Malgun Gothic", 18, System::Drawing::FontStyle::Bold));
-			this->loadChart->Location = System::Drawing::Point(28, 255);
+			this->loadChart->Location = System::Drawing::Point(146, 187);
 			this->loadChart->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
 			this->loadChart->Name = L"loadChart";
-			this->loadChart->Size = System::Drawing::Size(443, 53);
+			this->loadChart->Size = System::Drawing::Size(549, 53);
 			this->loadChart->TabIndex = 11;
 			this->loadChart->Text = L"Load Chart";
 			this->loadChart->UseVisualStyleBackColor = true;
@@ -177,7 +192,7 @@ namespace Project3 {
 			// 
 			this->runtimeLabel->AutoSize = true;
 			this->runtimeLabel->Font = (gcnew System::Drawing::Font(L"Malgun Gothic", 18, System::Drawing::FontStyle::Bold));
-			this->runtimeLabel->Location = System::Drawing::Point(80, 748);
+			this->runtimeLabel->Location = System::Drawing::Point(557, 30);
 			this->runtimeLabel->Name = L"runtimeLabel";
 			this->runtimeLabel->Size = System::Drawing::Size(146, 41);
 			this->runtimeLabel->TabIndex = 13;
@@ -188,11 +203,11 @@ namespace Project3 {
 			this->runtimeDisplay->Enabled = false;
 			this->runtimeDisplay->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->runtimeDisplay->Location = System::Drawing::Point(232, 744);
+			this->runtimeDisplay->Location = System::Drawing::Point(709, 26);
 			this->runtimeDisplay->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
 			this->runtimeDisplay->MaxLength = 9999;
 			this->runtimeDisplay->Name = L"runtimeDisplay";
-			this->runtimeDisplay->Size = System::Drawing::Size(183, 45);
+			this->runtimeDisplay->Size = System::Drawing::Size(208, 45);
 			this->runtimeDisplay->TabIndex = 14;
 			this->runtimeDisplay->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox1_TextChanged);
 			// 
@@ -200,7 +215,7 @@ namespace Project3 {
 			// 
 			this->zipInput->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->zipInput->Location = System::Drawing::Point(207, 111);
+			this->zipInput->Location = System::Drawing::Point(301, 26);
 			this->zipInput->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
 			this->zipInput->MaxLength = 5;
 			this->zipInput->Name = L"zipInput";
@@ -211,13 +226,13 @@ namespace Project3 {
 			// 
 			// zipList
 			// 
-			this->zipList->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 7, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->zipList->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 19.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->zipList->HideSelection = false;
-			this->zipList->Location = System::Drawing::Point(28, 162);
+			this->zipList->Location = System::Drawing::Point(146, 86);
 			this->zipList->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
 			this->zipList->Name = L"zipList";
-			this->zipList->Size = System::Drawing::Size(443, 86);
+			this->zipList->Size = System::Drawing::Size(769, 86);
 			this->zipList->TabIndex = 16;
 			this->zipList->UseCompatibleStateImageBehavior = false;
 			this->zipList->View = System::Windows::Forms::View::SmallIcon;
@@ -226,10 +241,10 @@ namespace Project3 {
 			// resetButton
 			// 
 			this->resetButton->Font = (gcnew System::Drawing::Font(L"Malgun Gothic", 18, System::Drawing::FontStyle::Bold));
-			this->resetButton->Location = System::Drawing::Point(28, 680);
+			this->resetButton->Location = System::Drawing::Point(723, 187);
 			this->resetButton->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
 			this->resetButton->Name = L"resetButton";
-			this->resetButton->Size = System::Drawing::Size(443, 53);
+			this->resetButton->Size = System::Drawing::Size(192, 53);
 			this->resetButton->TabIndex = 17;
 			this->resetButton->Text = L"Reset";
 			this->resetButton->UseVisualStyleBackColor = true;
@@ -239,7 +254,7 @@ namespace Project3 {
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(501, 800);
+			this->ClientSize = System::Drawing::Size(1270, 785);
 			this->Controls->Add(this->resetButton);
 			this->Controls->Add(this->zipList);
 			this->Controls->Add(this->zipInput);
@@ -318,13 +333,14 @@ namespace Project3 {
 		}
 	}
 	
-	private: System::Void fillChart() {
+	private: System::Void fillChart(bool& chartDrawn, String^& message) {
 
 		zipChart->Series->Clear();
+		System::Collections::Generic::List<String^> badZipCodes;
 
 		for (int i = 0; i < 6; i++) {
 			auto series = gcnew System::Windows::Forms::DataVisualization::Charting::Series(); // Create series
-			series->Name = "Income" + i;
+			series->Name = incomeRanges[i];
 			series->ChartType = System::Windows::Forms::DataVisualization::Charting::SeriesChartType::Bar; // Chart styling
 			zipChart->Series->Add(series); // Adding series
 		}
@@ -332,33 +348,54 @@ namespace Project3 {
 		for each (ListViewItem ^ i in zipList->Items) {
 			String^ currentZipcode = i->Text;
 			bool exists = false;
-			for (int s = 0; s < 6; s++) {
-				for each (DataVisualization::Charting::DataPoint ^ point in zipChart->Series["Income" + s]->Points) {
-					if (point->AxisLabel == currentZipcode) {
-						exists = true;
-						break;
-					}
-				}
-				if (exists) {
-					break;
-				}
-			}
+			//for (int s = 0; s < 6; s++) {
+			//	for each (DataVisualization::Charting::DataPoint ^ point in zipChart->Series["Income" + s]->Points) {
+			//		if (point->AxisLabel == currentZipcode) {
+			//			exists = true;
+			//			break;
+			//		}
+			//	}
+			//	if (exists) {
+			//		break;
+			//	}
+			//}
 
 			if (!exists) {
 				int currentZipcodeInt  = System::Convert::ToInt32(currentZipcode);
 				std::array<int, 6> zipIncomes; // Array to hold each income value
+				bool success = false;
 				if (this->keypairRadio->Checked) {
 					std::pair<bool, AssociationList::Container> result = keyPair->retrieve(currentZipcodeInt);
 					zipIncomes = result.second.incomes;// Retrieve income from respective data structure
+					success = result.first;
 				}
 				else if (this->hashtableRadio->Checked) {
 					std::pair<bool, HashMap::Container> result = hashMap->retrieve(currentZipcodeInt);
 					zipIncomes = result.second.incomes; // Retrieve income from respective data structure
+					success = result.first;
 				}
-				for (int i = 0; i < 6; i++) {
-					zipChart->Series["Income" + i]->Points->AddXY(currentZipcode, zipIncomes[i]); // Add data to chart, 6 bars for each zipcode
+				// list of all unsuccessful zipcodes
+				if (success) {
+					for (int i = 0; i < 6; i++) {
+						zipChart->Series[incomeRanges[i]]->Points->AddXY(currentZipcode, zipIncomes[i]); // Add data to chart, 6 bars for each zipcode
+					}
+				}
+				else {
+					badZipCodes.Add(currentZipcode);
 				}
 			}
+		}
+		if (badZipCodes.Count == 0) {
+			zipChart->Legends[0]->Enabled = true; // show legend
+			chartDrawn = true;
+		}
+		else {
+			message = "The following zip codes are invalid and were not shown: ";
+			for (int i = 0; i < badZipCodes.Count; i++) {
+				message += "\n" + badZipCodes[i];
+			}
+			chartDrawn = badZipCodes.Count == zipList->Items->Count ? false : true;
+			zipChart->Legends[0]->Enabled = chartDrawn;
 		}
 	}
 
@@ -367,19 +404,30 @@ namespace Project3 {
 	private: System::Void textBox2_TextChanged_1(System::Object^ sender, System::EventArgs^ e) {
 	}
 	private: System::Void loadChart_Click(System::Object^ sender, System::EventArgs^ e) {
+		bool chartDrawn = false;
+		String^ message = "";
 		Stopwatch^ stopwatch = gcnew Stopwatch(); // run time display
 		stopwatch->Start();
-		fillChart();
+		fillChart(chartDrawn, message);
 		stopwatch->Stop();
 		double timeElapsed = stopwatch->Elapsed.TotalMilliseconds;
 		String^ elapsedTime = String::Format("{0} ms", timeElapsed);
-		runtimeDisplay->Text = elapsedTime;
+		if (chartDrawn) {
+			runtimeDisplay->Text = elapsedTime;
+		}
+		if (message != "") {
+			MessageBox::Show(message);
+		}
 	}
 	private: System::Void resetButton_Click(System::Object^ sender, System::EventArgs^ e) {
 		for (int i = 0; i < 6; i++) {
-			zipChart->Series["Income" + i]->Points->Clear(); // clear chart
+			zipChart->Series[incomeRanges[i]]->Points->Clear(); // clear chart
 		}
 		this->zipList->Items->Clear(); // clear zip code list
+		zipChart->Legends[0]->Enabled = false; // hides legend
+		runtimeDisplay->Text = "";
 	}
+private: System::Void zipLabel_Click(System::Object^ sender, System::EventArgs^ e) {
+}
 };
 }
